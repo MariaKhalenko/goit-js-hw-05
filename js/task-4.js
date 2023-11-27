f49f6312bda1357496f35ed1342bd306b40dde7b
MariaKhalenko <134129926+MariaKhalenko@users.noreply.github.com>
2023-11-27 20:58:08 +0200

    goit-js-hw-05

--- js/task-4.js
+++ js/task-4.js
@@ -1,6 +1,8 @@
 const getTotalBalanceByGender = (users, gender) => {
-  return users.filter((user) => user.gender === gender).reduce((total, user) => total + user.balance, 0);
-}
+  return users
+    .filter((user) => user.gender === gender)
+    .reduce((sum, user) => sum + user.balance, 0);
+};
 
 const allUsers = [
   {
